bea405056db8b31ccc90186e18b45c0b84dd0bbb
Alex Zabib <azabib19@ssdsnassau.org>
2018-09-20 15:14:51 -0400

    Change in code

--- ASCIIArt/src/FirstArt.java
+++ ASCIIArt/src/FirstArt.java
@@ -2,7 +2,8 @@
 public class FirstArt {
 
 	public static void main(String[] args) {
-		System.out.println("\\Hello world\");
+		System.out.println("Hello World");
+		System.out.println("\\Hello World");
 
 	}
 
