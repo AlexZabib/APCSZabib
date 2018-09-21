1ddfe35053c317512ec08e1fefe17d00062fe230
Alex Zabib <azabib19@ssdsnassau.org>
2018-09-20 22:39:57 -0400

    Changed from Portal ASCII to Sonic ASCII

--- ASCIIArt/src/FirstArt.java
+++ ASCIIArt/src/FirstArt.java
@@ -2,9 +2,60 @@
 public class FirstArt {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("\\Hello World");
-
+		System.out.println("                             ...,?77??!~~~~!???77?<~.... ");
+		System.out.println("                        ..?7`                           `7!.. ");
+		System.out.println("                    .,=`          ..~7^`   I                  ?1. ");
+		System.out.println("       ........  ..^            ?`  ..?7!1 .               ...??7 ");
+		System.out.println("      .        .7`        .,777.. .I.    . .!          .,7! ");
+		System.out.println("      ..     .?         .^      .l   ?i. . .`       .,^ ");
+		System.out.println("       b    .!        .= .?7???7~.     .>r .      .= ");
+		System.out.println("       .,.?4         , .^         1        `     4... ");
+		System.out.println("        J   ^         ,            5       `         ?<. ");
+		System.out.println("       .%.7;         .`     .,     .;                   .=. ");
+		System.out.println("       .+^ .,       .%      MML     F       .,             ?, ");
+		System.out.println("        P   ,,      J      .MMN     F        6               4. ");
+		System.out.println("        l    d,    ,       .MMM!   .t        ..               ,, ");
+		System.out.println("        ,    JMa..`         MMM`   .         .!                .; ");
+		System.out.println("         r   .M#            .M#   .%  .      .~                 ., ");
+		System.out.println("       dMMMNJ..!                 .P7!  .>    .         .         ,, ");
+		System.out.println("       .WMMMMMm  ?^..       ..,?! ..    ..   ,  Z7`        `?^..  ,, ");
+		System.out.println("          ?THB3       ?77?!        .Yr  .   .!   ?,              ?^C ");
+		System.out.println("            ?,                   .,^.` .%  .^      5. ");
+		System.out.println("              7,          .....?7     .^  ,`        ?. ");
+		System.out.println("                `<.                 .= .`'           1 ");
+		System.out.println("                ....dn... ... ...,7..J=!7,           ., ");
+		System.out.println("             ..=     G.,7  ..,o..  .?    J.           F ");
+		System.out.println("           .J.  .^ ,,,t  ,^        ?^.  .^  `?~.      F ");
+		System.out.println("          r %J. $    5r J             ,r.1      .=.  .% ");
+		System.out.println("          r .77=?4.    ``,     l ., 1  .. <.       4., ");
+		System.out.println("          .$..    .X..   .n..  ., J. r .`  J.       `' ");
+		System.out.println("        .?`  .5        `` .%   .% .' L.'    t ");
+		System.out.println("        ,. ..1JL          .,   J .$.?`      . ");
+		System.out.println("                1.          .=` ` .J7??7<.. .; ");
+		System.out.println("                 JS..    ..^      L        7.: ");
+		System.out.println("                   `> ..       J.  4. ");
+		System.out.println("                    +   r `t   r ~=..G. ");
+		System.out.println("                    =   $  ,.  J ");
+		System.out.println("                    2   r   t  .; ");
+		System.out.println("              .,7!  r   t`7~..  j.. ");
+		System.out.println("              j   7~L...$=.?7r   r ;?1. ");
+		System.out.println("               8.      .=    j ..,^   .. ");
+		System.out.println("              r        G              . ");
+		System.out.println("            .,7,        j,           .>=. ");
+		System.out.println("         .J??,  `T....... %             .. ");
+		System.out.println("      ..^     <.  ~.    ,.             .D ");
+		System.out.println("    .?`        1   L     .7.........?Ti..l ");
+		System.out.println("   ,`           L  .    .%    .`!       `j, ");
+		System.out.println(" .^             .  ..   .`   .^  .?7!?7+. 1 ");
+		System.out.println(".`              .  .`..`7.  .^  ,`      .i.; ");
+		System.out.println(".7<..........~<<3?7!`    4. r  `          G% ");
+		System.out.println("                          J.` .!           % ");
+		System.out.println("                            JiJ           .` ");
+		System.out.println("                              .1.         J ");
+		System.out.println("                                 ?1.     .'     ");
+		System.out.println("                                     7<..%");
+		System.out.println("Sonic the Hedgehog from https://www.asciiart.eu/video-games/sonic-the-hedgehog");
+		
 	}
 
 }
