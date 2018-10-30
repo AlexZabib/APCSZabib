00a15d0c6124fb82611de9fb8f913380f1d29351
Alex Zabib <azabib19@ssdsnassau.org>
2018-10-30 15:30:41 -0400

    Almost done with check

--- TicTacToe/src/AttemptAtTTT.java
+++ TicTacToe/src/AttemptAtTTT.java
@@ -2,20 +2,22 @@ import java.util.Scanner;
 
 public class AttemptAtTTT
 {
-	static char[][] board = {
-			{' ', ' ', ' '},
-			{' ', ' ', ' '},
-			{' ', ' ', ' '}
-	};
+	static char[] board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 	
 	public static void main(String[] args) 
 	{
-			System.out.println("Columns go from 0 to 2 and Rows go from 0 to 2. 0,0 is Top Left and 2,2 is Bottom Right.");
+			System.out.println("Board locations are as follows");
+			System.out.println(" 0 | 1 | 2 ");
+			System.out.println("-----------");
+			System.out.println(" 3 | 4 | 5 ");
+			System.out.println("-----------");
+			System.out.println(" 6 | 7 | 8 " +" \n");
 			
 			printBoard();
 			for(int k = 0; k<=5; k++)
 			{
 				cycle();
+				checkW();
 					
 			}
 			
@@ -23,50 +25,46 @@ public class AttemptAtTTT
 	
 	public static void printBoard()
 	{
-		System.out.println(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " ");
+		System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2] + " ");
 		System.out.println("-----------");
-		System.out.println(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " ");
+		System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5] + " ");
 		System.out.println("-----------");
-		System.out.println(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] +" ");
+		System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8] +" ");
 	}
 	public static char playX() 
 	{
 		Scanner input = new Scanner(System.in);
-		System.out.println("Pick x Location");
-		int x = input.nextInt();
-		System.out.println("Pick y Location");
-		int y = input.nextInt();
+		System.out.println("Pick Location");
+		int n = input.nextInt();
 		
-		if(board[x][y] == 'Y' || board[x][y] == 'X')
+		if(board[n] == 'O' || board[n] == 'X')
 		{
 			System.out.println("Invalid Move Pick Again");
 			playX();
 			
 		}
 		else {
-			return board[x][y] = 'X';
+			return board[n] = 'X';
 		}
-		return board[x][y] = board[x][y];
+		return board[n] = board[n];
 			
 	}
 	public static char playY()
 	{
 		Scanner input = new Scanner(System.in);
-		System.out.println("Pick x Location");
-		int x = input.nextInt();
-		System.out.println("Pick y Location");
-		int y = input.nextInt();
+		System.out.println("Pick Location");
+		int n = input.nextInt();
 
-		if(board[x][y] == 'Y' || board[x][y] == 'X')
+		if(board[n] == 'O' || board[n] == 'X')
 		{
 			System.out.println("Invalid Move Pick Again");
 			playY();
 			
 		}
 		else {
-			return board[x][y] = 'Y';
+			return board[n] = 'O';
 		}
-		return board[x][y] = board[x][y];
+		return board[n] = board[n];
 	}
 	public static void cycle()
 	{
@@ -75,9 +73,52 @@ public class AttemptAtTTT
 		playY();
 		printBoard();
 	}
-	/*public static char checkW()
+	public static char checkW()
 	{
-	}*/
+		String line1 = "" + board[0] + board[1] + board[2];
+		String line2 = "" + board[3] + board[4] + board[5];
+		String line3 = "" + board[6] + board[7] + board[8];
+		String line4 = "" + board[0] + board[3] + board[6];
+		String line5 = "" + board[1] + board[4] + board[7];
+		String line6 = "" + board[2] + board[5] + board[8];
+		String line7 = "" + board[0] + board[4] + board[8];
+		String line8 = "" + board[2] + board[4] + board[6];
+		
+		if(line1.equals("XXX"))
+			return 'X';
+		else if(line2.equals("XXX"))
+			return 'X';
+		else if(line3.equals("XXX"))
+			return 'X';
+		else if(line4.equals("XXX"))
+			return 'X';
+		else if(line5.equals("XXX"))
+			return 'X';
+		else if(line6.equals("XXX"))
+			return 'X';
+		else if(line7.equals("XXX"))
+			return 'X';
+		else if(line8.equals("XXX"))
+			return 'X';
+		else if(line1.equals("YYY"))
+			return 'Y';
+		else if(line2.equals("YYY"))
+			return 'Y';
+		else if(line3.equals("YYY"))
+			return 'Y';
+		else if(line4.equals("YYY"))
+			return 'Y';
+		else if(line5.equals("YYY"))
+			return 'Y';
+		else if(line6.equals("YYY"))
+			return 'Y';
+		else if(line7.equals("YYY"))
+			return 'Y';
+		else if(line8.equals("YYY"))
+			return 'Y';
+		else
+			return 'T';
+	}
 	
 	
 	
