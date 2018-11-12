c5ac97447b947e09bc77314c7e72e4bc7c9fc5e8
Alex Zabib <azabib19@ssdsnassau.org>
2018-11-12 15:32:14 -0500

    Attampt at first homework assignment

---
+++ Homework/src/CodingBatP199484.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CodingBatP199484 {
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Pick a number: ");
+		int num = input.nextInt();
+		tenRun(num);
+
+	}
+	
+	public static void tenRun(int num)
+	{
+		
+	}
+
+}
