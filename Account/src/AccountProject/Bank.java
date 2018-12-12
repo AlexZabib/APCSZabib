ff7cf46453bdad1e8a2916932aebf7d3a1fb3937
Alex Zabib <azabib19@ssdsnassau.org>
2018-12-12 14:24:41 -0500

    Update Bank.java

--- Account/src/AccountProject/Bank.java
+++ Account/src/AccountProject/Bank.java
@@ -1,5 +1,28 @@
 package AccountProject;
 
 public class Bank {
+	private Customer[] customers = new Customer[100];
+	private int numberOfCustomers = customers.length;
+	
+	public Bank()
+	{
+		numberOfCustomers = 0;
+	}
+	
+	public void addCustomer(String f, String l, int initBalance)
+	{
+		Customer c = new Customer(f, l, initBalance);
+		numberOfCustomers = numberOfCustomers + 1; //updates the number of customer objects.
+		customers[numberOfCustomers] = c; //placed the new customer object into the array.
+	}
 
+	public int getNumofCustomers()
+	{
+		return numberOfCustomers;
+	}
+	
+	public Customer getCustomer(int index)
+	{
+		return customers[index];
+	}
 }
