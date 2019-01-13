ba14ae51946bda4e920107d8b366f10e3c68027c
AlexZabib <azabib19@ssdsnassau.org>
2019-01-13 12:56:38 -0500

    Update FRQuiz1204.java

--- Home Comp/src/FRQuiz1204.java
+++ Home Comp/src/FRQuiz1204.java
@@ -7,7 +7,7 @@ public class FRQuiz1204
 		fibo[0] = 0; //declares first number in sequence
 		fibo[1] = 1; //declares second number in sequence
 		System.out.println(fibo[0] + "\n" + fibo[1]); //print first two numbers of sequence
-		for(int i = 2; i<fibo.length; i++) //set up for loop to print all number in sequence from third nuymber to array limit
+		for(int i = 2; i<fibo.length; i++) //set up for loop to print all number in sequence from third number to array limit
 		{
 			System.out.println(computeFibo(fibo, i)); //prints computed fibonacci number
 		}
