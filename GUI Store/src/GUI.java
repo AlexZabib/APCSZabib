8e4a3133bcdfa96e3b5a13abf8fe81704b1c5b9b
Alex Zabib <azabib19@ssdsnassau.org>
2019-02-27 15:25:37 -0500

    Started GUI

---
+++ GUI Store/src/GUI.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI {
+	JFrame myFrame = new JFrame();
+	JPanel panel1 = new JPanel();
+	JPanel panel2 = new JPanel();
+	JPanel panel3 = new JPanel();
+	JButton S = new JButton();
+	JButton M = new JButton();
+	JButton L = new JButton();
+	JButton XL = new JButton();
+	JTextField style = new JTextField();
+	JTextField color = new JTextField();
+	JTextField price = new JTextField();
+	JTextField amount = new JTextField();
+	JTextField id = new JTextField();
+	JLabel list = new JLabel();
+	JButton save = new JButton();
+	
+	
+	public void read() {
+		
+	}
+	public void save() {
+		
+	}
+	public static void main (String[] args)
+	{
+		GUI g = new GUI();
+	}
+}
