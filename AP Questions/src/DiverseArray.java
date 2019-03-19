28f79ed3377f30dc58eddf275dd760df876d0927
Alex Zabib <azabib19@ssdsnassau.org>
2019-03-19 15:22:07 -0400

    AP Questions completed

--- AP Questions/src/DiverseArray.java
+++ AP Questions/src/DiverseArray.java
@@ -4,10 +4,18 @@ public class DiverseArray {
 	public static void main(String[] args) {
 		int arr1[] = {1,3,2,7,3};
 		int arr2[][] = {{1,2,3,4,5,6,7,8,9},
-						{1,2,3,4,5,6,7,8,0},
-						{1,2,3,4,5,6,7,0,0}};
+						{1,2,3,4,5,6,7,8,9},
+						{1,2,3,4,5,6,7,8,0}
+					   };
 		arraySum(arr1);
-		System.out.println(rowSum(arr2));
+		
+		int[] ans = rowSum(arr2);
+		for(int i = 0; i<ans.length;i++)
+		{
+			System.out.println(ans[i] + " ");	
+		}
+		
+		System.out.println(isDiverse(arr2));
 		
 		
 
@@ -24,19 +32,28 @@ public class DiverseArray {
 	
 	public static int[] rowSum(int[][] a1)
 	{
-		int rowCount = 0;
+		int[] a2 = new int [a1.length];
 		int sum = 0;
-		int[] a2 = new int [rowCount];
 		for(int i = 0; i<a1.length; i++)
 		{
 			for(int k = 0; k<a1[i].length; k++)
 			{
-				sum = sum + a1[rowCount][i];
+				sum = sum + a1[i][k];
 			}
 			a2[i] = sum;
-			rowCount++;
+			sum = 0;
 		}
 		return a2;
+	}
+	
+	public static boolean isDiverse(int[][] a2)
+	{
+		int[] ans = rowSum(a2);
+		for(int i = 0; i<ans.length;i++)
+			for(int k = i+1; k<ans.length; k++)
+					if(ans[i] == ans[k])
+						return false;
+		return true;
 	}
 
 }
